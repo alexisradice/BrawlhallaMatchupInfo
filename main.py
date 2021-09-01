7659a2b8666f9116e9500676afbb3a68ab343e60
Alexis Radice <aradice@dotscreen.com>
2021-09-01 04:45:59 +0200

    cleaned version

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import tkinter as tk
+import tkinter as ttk
+from queue import Queue
+from threading import Thread
+# from main import detect_brawlhalla
+import requests
+from pathlib import Path
 import os
 import cv2
 import numpy as np
@@ -5,38 +12,40 @@ import pyautogui
 import time
 import easyocr
 
-reader = easyocr.Reader(['en'])  # need to run only once to load model into memory
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./img/assets")
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+# need to run only once to load model into memory
+reader = easyocr.Reader(['en'])
+
 dir = os.path.dirname(os.path.abspath(__file__))
 
 def ocr_core(img):
-    text = reader.readtext(img, detail = 0)
+    text = reader.readtext(img, detail=0)
     return text
-
 
 def get_greyscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-def remove_noise(image):
-    return cv2.medianBlur(image, 5)
-
-
-def thresholding(image):
-    #return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    return cv2.threshold(image,55,255,cv2.THRESH_BINARY)
-    #return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-
+end = False
 
 def detect_brawlhalla(queue=None):
 
-    end = False
     while not end:
         try:
             ok = False
             pathcodeLoading = dir + "/img/imgLoading.jpg"
             pathcodeBattle = dir + "/img/imgBattle.jpg"
-            
-            textarealocation = pyautogui.locateOnScreen(pathcodeLoading, confidence=0.9)
+
+            textarealocation = pyautogui.locateOnScreen(
+                pathcodeLoading, confidence=0.9)
             if textarealocation is not None:
                 print("Detection OK")
                 ok = True
@@ -44,11 +53,10 @@ def detect_brawlhalla(queue=None):
                 image = pyautogui.screenshot()
                 image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
                 cv2.imwrite(pathcodeBattle, image)
-                #   pyautogui.write(code)
 
                 imgBattle = cv2.imread(pathcodeBattle)
                 imgBattle = get_greyscale(imgBattle)
-                # imgBattle = thresholding(imgBattle)
+
                 y = 263
                 x = 1208
                 h = 80
@@ -92,8 +100,6 @@ def detect_brawlhalla(queue=None):
                     tagPlayer = "/"
                     clanPlayer = "/"
 
-
-
                 print("Character Player: " + persoPlayer + ", Tag Player: " + tagPlayer + ", Name Player: " + namePlayer + ", Clan Player: " + clanPlayer)
                 finalList = [persoPlayer, tagPlayer, namePlayer, clanPlayer]
 
@@ -105,152 +111,482 @@ def detect_brawlhalla(queue=None):
             print("bug main")
 
 
+brawlIdClient = 0
 
-#detect_brawlhalla()
+def mainFrame():
 
-# TESTS
+    def infosBrawlRecuperation():
 
-if __name__ == '__main__':
-    # dir_path = os.path.dirname(os.path.realpath(__file__))
-    dir = os.path.dirname(os.path.abspath(__file__))
-
-    pathcodeLoading = dir + "/img/imgLoading.jpg"
-    pathcodeBattle = dir + "/img/imgBattle.jpg"
-    pathcodeTest1 = dir + "/img/imgTest1.jpg"
-    pathcodeTest2 = dir + "/img/imgTest2.jpg"
-
-    imgTest1 = cv2.imread(pathcodeBattle)
-
-    imgTest1 = get_greyscale(imgTest1)
-    # imgTest1 = thresholding(imgTest1)
-    # imgTest1 = remove_noise(imgTest1)
-
-    y = 263
-    x = 1208
-    h = 80
-    w = 280
-    start_point = (1200, 264)
-    end_point = (1250, 290)
-    color = (0, 0, 0)
-    thickness = -1
-    crop = cv2.rectangle(imgTest1, start_point, end_point, color, thickness)
-    crop = imgTest1[y:y + h, x:x + w]
-    #crop = cv2.resize(crop, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    # ret,crop = cv2.threshold(crop,120,255,cv2.THRESH_BINARY)
-    cv2.imshow('Image', crop)
-    cv2.waitKey(0)
-
-    imgTest2 = cv2.imread(pathcodeTest2)
-
-    imgTest2 = get_greyscale(imgTest2)
-    # imgTest2 = thresholding(imgTest2)
-    # imgTest2 = remove_noise(imgTest2)
-
-    y = 263
-    x = 1208
-    h = 80
-    w = 280
-    start_point = (1200, 264)
-    end_point = (1250, 290)
-    color = (0, 0, 0)
-    thickness = -1
-    crop2 = cv2.rectangle(imgTest2, start_point, end_point, color, thickness)
-    crop2 = imgTest2[y:y + h, x:x + w]
-    crop2 = cv2.resize(crop2, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    # ret,crop2 = cv2.threshold(crop,120,255,cv2.THRESH_BINARY)
-    cv2.imshow('Image', crop2)
-    cv2.waitKey(0)
-
-    imgTest1 = cv2.imread(pathcodeTest1)
-    y = 50
-    x = 860
-    h = 580
-    w = 200
-    crop3 = imgTest1[y:y + h, x:x + w]
-    cv2.imshow('Image', crop3)
-    cv2.waitKey(0)
-    # cv2.imwrite('imgLoading.jpg',crop3) #for saving the crop image
-
-    listInfoJ1 = ocr_core(crop)
-    listInfoJ2 = ocr_core(crop2)
-    detectImageLoading = ocr_core(crop3)
-
-    print(listInfoJ1)
-    print(listInfoJ2)
-
-    persoJ1 = ""
-    tagJ1 = ""
-    nameJ1 = ""
-    clanJ1 = ""
-    persoJ2 = ""
-    tagJ2 = ""
-    nameJ2 = ""
-    clanJ2 = ""
+        if not q.empty():
+            finalList = q.get()
+            # print(finalList)
 
 
+            q.task_done()
+
+            print(brawlIdClient)
+
+            try:
+
+                linkEloClient = "https://brawlhalla-api.herokuapp.com/v1/ranked/id?brawlhalla_id={}".format(
+                    brawlIdClient)
+                responseEloClient = requests.get(linkEloClient)
+                apiResultEloClient = responseEloClient.json()
+                EloClient = apiResultEloClient["data"]["rating"]
+                print(finalList[2], EloClient, brawlIdClient)
+
+                linkAPI = "http://localhost:8080/api/brawl/{}&{}&{}".format(
+                    finalList[2], EloClient, brawlIdClient)
+                response = requests.get(linkAPI)
+                apiResult = response.json()
+                levelOpponent = apiResult['playerStatsJSON']['level']
+                ratingOpponent = apiResult['playerRankedJSON']['rating']
+                peakRatingOpponent = apiResult['playerRankedJSON']['peak_rating']
+                mainCharacterOpponent = apiResult['playerOtherJSON']['mainCharacter']
+                mainWeaponOpponent = apiResult['playerOtherJSON']['mainWeapon']
+                trueLevelOpponent = apiResult['playerOtherJSON']['trueLevel']
+                passiveAgressiveOpponent = apiResult['playerOtherJSON']['passiveAgressive']
+
+                levelOpponentEntry.delete(0, tk.END)
+                levelOpponentEntry.insert(0, levelOpponent)
+
+                ratingOpponentEntry.delete(0, tk.END)
+                ratingOpponentEntry.insert(0, ratingOpponent)
+
+                peakRatingOpponentEntry.delete(0, tk.END)
+                peakRatingOpponentEntry.insert(0, peakRatingOpponent)
+
+                mainCharacterOpponentEntry.delete(0, tk.END)
+                mainCharacterOpponentEntry.insert(0, mainCharacterOpponent)
+
+                mainWeaponOpponentEntry.delete(0, tk.END)
+                mainWeaponOpponentEntry.insert(0, mainWeaponOpponent)
+
+                trueLevelOpponentEntry.delete(0, tk.END)
+                trueLevelOpponentEntry.insert(0, trueLevelOpponent)
+
+                passiveAgressiveOpponentEntry.delete(0, tk.END)
+                passiveAgressiveOpponentEntry.insert(0, passiveAgressiveOpponent)
+
+            except Exception as e:
+                print(e)
+
+        root.after(100, infosBrawlRecuperation)
+
+    window = tk.Tk()
+
+    window.geometry("406x570")
+    window.title("Brawlhalla Matchup Infos")
+    window.configure(bg="#1F1A1A")
+
+    # FontOfEntryList = tk.font.Font(family="Calibri", size=12)
+
+    canvas = tk.Canvas(
+        window,
+        bg="#1F1A1A",
+        height=570,
+        width=406,
+        bd=0,
+        highlightthickness=0,
+        relief="ridge"
+    )
+
+    canvas.place(x=0, y=0)
+    playerNameText = canvas.create_text(
+        14.000000000000014,
+        165.0,
+        anchor="nw",
+        text="PlayerName",
+        fill="#FFFFFF",
+        font=("Roboto", 36 * -1)
+    )
+
+    # canvas.itemcongig(playerNameText, text="text has changed!")
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        515.0,
+        379.0,
+        548.0,
+        fill="#000000",
+        outline="")
+
+    canvas.create_text(
+        39.934211730957045,
+        521.0,
+        anchor="nw",
+        text="Passive / Agressive :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        469.0,
+        379.0,
+        502.0,
+        fill="#000000",
+        outline="")
+
+    canvas.create_text(
+        39.934211730957045,
+        475.0,
+        anchor="nw",
+        text="True Level :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        423.0,
+        379.0,
+        456.0,
+        fill="#000000",
+        outline="")
+
+    canvas.create_text(
+        39.934211730957045,
+        429.0,
+        anchor="nw",
+        text="Level :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        377.0,
+        379.0,
+        410.0,
+        fill="#000000",
+        outline="")
+
+    canvas.create_text(
+        39.934211730957045,
+        383.0,
+        anchor="nw",
+        text="Main Weapon :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        331.0,
+        379.0,
+        364.0,
+        fill="#000000",
+        outline="")
+
+    canvas.create_text(
+        39.934211730957045,
+        337.0,
+        anchor="nw",
+        text="Main Legend :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        285.0,
+        379.0,
+        318.0,
+        fill="#000000",
+        outline="")
+
+    canvas.create_text(
+        39.934211730957045,
+        291.0,
+        anchor="nw",
+        text="Elo Max :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        26.000000000000014,
+        239.0,
+        379.0,
+        272.0,
+        fill="#000000",
+        outline="")
+
+    ratingOpponent = tk.StringVar()
+    entry_image_ratingOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_1.png"))
+    entry_bg_ratingOpponent = canvas.create_image(
+        226.5,
+        252.5,
+        image=entry_image_ratingOpponent
+    )
+    ratingOpponentEntry = ttk.Entry(
+        textvariable=ratingOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    ratingOpponentEntry.place(
+        x=146.0,
+        y=242.0,
+        width=161.0,
+        height=25.0
+    )
+
+    peakRatingOpponent = tk.StringVar()
+    entry_image_peakRatingOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_2.png"))
+    entry_bg_peakRatingOpponent = canvas.create_image(
+        200.5,
+        298.5,
+        image=entry_image_peakRatingOpponent
+    )
+    peakRatingOpponentEntry = ttk.Entry(
+        textvariable=peakRatingOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",  
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    peakRatingOpponentEntry.place(
+        x=120.00000000000001,
+        y=288.0,
+        width=161.0,
+        height=25.0
+    )
+
+    mainCharacterOpponent = tk.StringVar()
+    entry_image_mainCharacterOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_3.png"))
+    entry_bg_mainCharacterOpponent = canvas.create_image(
+        241.5,
+        344.5,
+        image=entry_image_mainCharacterOpponent
+    )
+    mainCharacterOpponentEntry = ttk.Entry(
+        textvariable=mainCharacterOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    mainCharacterOpponentEntry.place(
+        x=161.0,
+        y=334.0,
+        width=161.0,
+        height=25.0
+    )
+
+    mainWeaponOpponent = tk.StringVar()
+    entry_image_mainWeaponOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_4.png"))
+    entry_bg_mainWeaponOpponent = canvas.create_image(
+        247.5,
+        390.5,
+        image=entry_image_mainWeaponOpponent
+    )
+    mainWeaponOpponentEntry = ttk.Entry(
+        textvariable=mainWeaponOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    mainWeaponOpponentEntry.place(
+        x=167.0,
+        y=380.0,
+        width=161.0,
+        height=25.0
+    )
+    levelOpponent = tk.StringVar()
+    entry_image_levelOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_5.png"))
+    entry_bg_levelOpponent = canvas.create_image(
+        180.5,
+        436.5,
+        image=entry_image_levelOpponent
+    )
+    levelOpponentEntry = ttk.Entry(
+        textvariable=levelOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    levelOpponentEntry.place(
+        x=100.00000000000001,
+        y=426.0,
+        width=161.0,
+        height=25.0
+    )
+
+    trueLevelOpponent = tk.StringVar()
+    entry_image_trueLevelOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_6.png"))
+    entry_bg_trueLevelOpponent = canvas.create_image(
+        222.5,
+        482.5,
+        image=entry_image_trueLevelOpponent
+    )
+    trueLevelOpponentEntry = ttk.Entry(
+        textvariable=trueLevelOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    trueLevelOpponentEntry.place(
+        x=142.0,
+        y=472.0,
+        width=161.0,
+        height=25.0
+    )
+
+    passiveAgressiveOpponent = tk.StringVar()
+    entry_image_passiveAgressiveOpponent = tk.PhotoImage(
+        file=relative_to_assets("entry_7.png"))
+    entry_bg_passiveAgressiveOpponent = canvas.create_image(
+        295.5,
+        528.5,
+        image=entry_image_passiveAgressiveOpponent
+    )
+    passiveAgressiveOpponentEntry = ttk.Entry(
+        textvariable=passiveAgressiveOpponent,
+        bd=0,
+        bg="#000000",
+        # disabledbackground="#000000",
+        fg="#ffffff",
+        # disabledforeground="#ffffff",
+        # state="readonly",
+        # state="disabled",
+        font=("Calibri", 20),
+        highlightthickness=0
+    )
+    passiveAgressiveOpponentEntry.place(
+        x=215.0,
+        y=518.0,
+        width=161.0,
+        height=25.0
+    )
+
+    canvas.create_text(
+        39.934211730957045,
+        245.0,
+        anchor="nw",
+        text="Current Elo :",
+        fill="#E9EE23",
+        font=("Roboto Bold", 18 * -1)
+    )
+
+    canvas.create_rectangle(
+        325.0,
+        14.0,
+        385.0,
+        74.0,
+        fill="#000000",
+        outline="")
+
+    button_image_1 = tk.PhotoImage(
+        file=relative_to_assets("button_1.png"))
+    button_1 = tk.Button(
+        image=button_image_1,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: print("button_1 clicked"),
+        relief="flat"
+    )
+    button_1.place(
+        x=325.0,
+        y=14.0,
+        width=60.0,
+        height=60.0
+    )
+
+    canvas.create_rectangle(
+        19.000000000000014,
+        18.0,
+        169.0,
+        161.0,
+        fill="#837171",
+        outline="")
+
+    canvas.create_rectangle(
+        14.000000000000014,
+        217.0,
+        385.0,
+        223.0,
+        fill="#837171",
+        outline="")
+
+    window.resizable(False, False)
+
+    infosBrawlRecuperation()
+    window.mainloop()
+    global end
+    end = True
+
+q = Queue()
+t = Thread(target=detect_brawlhalla, args=(q,))
+
+def validateBrawlID():
+    t.start()
+
+    global brawlIdClient
+    brawlIdClient = brawlID.get()
+    # frame.pack_forget()
+    root.destroy()
+    mainFrame()
+    
 
 
 
-    # J1
+root = tk.Tk()
+root.title('Brawlhalla Matchup Infos')
+root.geometry("300x100")
 
-    if (len(listInfoJ1) == 4):
-        persoJ1 = listInfoJ1[0].lower().capitalize()
-        tagJ1 = listInfoJ1[1]
-        nameJ1 = listInfoJ1[2]
-        clanJ1 = listInfoJ1[3]
+frame = ttk.Frame(root)
+frame.pack()
 
-    if (len(listInfoJ1) == 3):
-        persoJ1 = listInfoJ1[0].lower().capitalize()
-        for i in range(len(listInfoJ1)):
-            if (listInfoJ1[2][0] == '<' and listInfoJ1[2][len(listInfoJ1[i]) - 1] == '>'):
-                clanJ1 = listInfoJ1[2]
-                nameJ1 = listInfoJ1[1]
-                tagJ1 = "/"
-            else:
-                tagJ1 = listInfoJ1[1]
-                nameJ1 = listInfoJ1[2]
-                clanJ1 = "/"
+characterPlayer = tk.StringVar()
+brawlIDLabel = ttk.Label(frame, width = 20, text = 'Enter your Brawlhalla ID : ')
+brawlIDLabel.grid(column = 0, row = 0)
+brawlID = tk.StringVar()
+brawlIDEntry = ttk.Entry(frame, width = 20, textvariable = brawlID)
+brawlIDEntry.grid(column = 0, row = 1)
+validateButton = ttk.Button(frame, width = 20, text = 'Validate', command = validateBrawlID)
+validateButton.grid(column = 0, row = 2)
 
-    if (len(listInfoJ1) == 2):
-        persoJ1 = listInfoJ1[0].lower().capitalize()
-        nameJ1 = listInfoJ1[1]
-        tagJ1 = "/"
-        clanJ1 = "/"
-
-
-
-
-
-
-
-    # J2
-
-    if (len(listInfoJ2) == 4):
-        persoJ2 = listInfoJ2[0].lower().capitalize()
-        tagJ2 = listInfoJ2[1]
-        nameJ2 = listInfoJ2[2]
-        clanJ2 = listInfoJ2[3]
-
-    if (len(listInfoJ2) == 3):
-        persoJ2 = listInfoJ2[0].lower().capitalize()
-        for i in range(len(listInfoJ2)):
-            if (listInfoJ2[2][0] == '<' and listInfoJ2[2][len(listInfoJ2[i]) - 1] == '>'):
-                clanJ2 = listInfoJ2[2]
-                nameJ2 = listInfoJ2[1]
-                tagJ2 = "/"
-            else:
-                tagJ2 = listInfoJ2[1]
-                nameJ2 = listInfoJ2[2]
-                clanJ2 = "/"
-
-    if (len(listInfoJ2) == 2):
-        persoJ2 = listInfoJ2[0].lower().capitalize()
-        nameJ2 = listInfoJ2[1]
-        tagJ2 = "/"
-        clanJ2 = "/"
-
-
-
-    print("Character J1: " + persoJ1 + ", Tag J1: " + tagJ1 + ", Name J1: " + nameJ1 + ", Clan J1: " + clanJ1)
-    print("Character J2: " + persoJ2 + ", Tag J2: " + tagJ2 + ", Name J2: " + nameJ2 + ", Clan J2: " + clanJ2)
+root.mainloop()
 
